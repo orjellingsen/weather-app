cb25b097062431ef095630286d8dd6beb529f8e9
Ørjan Ellingsen <orjellingsen@gmail.com>
2018-09-25 18:40:56 +0200

    cra

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ReactJS Bergen Workshop</h1>
+      </div>
+    )
+  }
+}
+
+export default App
